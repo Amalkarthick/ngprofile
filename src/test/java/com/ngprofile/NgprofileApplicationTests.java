46c017d95f36deab87ea3a6b1d5932298be9bc2b
Amalkarthickeyan <amalkarthick12@gmail.com>
2025-06-09 21:47:48 +0530

    intial commit

---
+++ src/test/java/com/ngprofile/NgprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
